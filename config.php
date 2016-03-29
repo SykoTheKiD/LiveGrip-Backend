ca7038c05fbdca53315415523f4be5e0a4ce41f6
Jay SyKo <jay@jaysyko.com>
2016-03-29 14:08:23 -0400

    Minor changes

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $host_name  = "localhost";
 $database   = "livegrip";
 $user_name  = "root";
-$password   = "1Drizzydrake!";
+$password   = "1Drizzydrake";
 $conn = mysqli_connect($host_name, $user_name, $password, $database);
 if (mysqli_connect_errno()){
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
--- newuser.php
+++ newuser.php
@@ -3,17 +3,21 @@
 	$user_username = $_POST[$USERNAME_KEY];
 	$user_password = $_POST[$PASSWORD_KEY];
 
-	$sql = "INSERT INTO users (username, password) VALUES ('$user_username', '$user_password')";
+	if($user_username != null && $user_password != null){
+		$sql = "INSERT INTO users (username, password) VALUES ('$user_username', '$user_password')";
 
-	$success = null;
-	$error = null;
-	if ($conn->query($sql) === TRUE) {
-    	$success = true;
-	} else {
-		$success = false;
-    	$error = $conn->error;
+		$success = null;
+		$error = null;
+		if ($conn->query($sql) === TRUE) {
+	    	$success = true;
+		} else {
+			$success = false;
+	    	$error = $conn->error;
+		}
+
+		$rows = array("success" => $success, "payload" => $error);
+		echo json_encode($rows);
+	}else{
+		echo json_encode(array('ERROR' => "NULL VALUES"));
 	}
-
-	$rows = array("success" => $success, "payload" => $error);
-	echo json_encode($rows);
 ?>
