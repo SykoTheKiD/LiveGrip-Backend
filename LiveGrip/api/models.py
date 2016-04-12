d990f60297c45736859aa68c6c3b2c1110402c1f
Jay SyKo <jay@jaysyko.com>
2016-04-11 21:26:02 -0400

    Added Custom User Model

--- LiveGrip/api/models.py
+++ LiveGrip/api/models.py
@@ -1,6 +1,10 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+	profile_image = models.CharField(max_length=300)
 
 # Create your models here.
 # class User(models.Model):
