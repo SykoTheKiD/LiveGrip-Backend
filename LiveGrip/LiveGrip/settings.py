ecdde330576c32ae4463073de358d2fcb8b5a11e
Jay SyKo <jay@jaysyko.com>
2016-04-25 11:03:04 -0400

    Update Date Format for Events

--- LiveGrip/LiveGrip/settings.py
+++ LiveGrip/LiveGrip/settings.py
@@ -75,7 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'LiveGrip.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
--- LiveGrip/api/serializers.py
+++ LiveGrip/api/serializers.py
@@ -5,7 +5,7 @@ from models import Event, User, Message
 class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
-		fields = ('username', 'profile_image')
+		fields = ('id','username', 'profile_image')
 
 	def create(self, validated_data):
 		user = None
@@ -26,9 +26,11 @@ class UserSerializer(serializers.ModelSerializer):
 			
 
 class EventSerializer(serializers.ModelSerializer):
+	start_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+	end_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
 	class Meta:
 		model = Event
-		fields = ('name', 'info', 'image','location', 'start_time', 'end_time', 'match_card')
+		fields = ('id','name', 'info', 'image','location', 'start_time', 'end_time', 'match_card')
 
 
 class GetMessageSerializer(serializers.ModelSerializer):
